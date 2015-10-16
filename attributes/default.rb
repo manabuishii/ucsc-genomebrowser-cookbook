d0645d75ae74967e7bc6434ce5547d3b083d883a
manabuishii <manabu.ishii.rb@gmail.com>
2015-10-16 17:04:23 +0900

    update jksrc.zip hash value

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:ucscGenomeBrowser][:jksrc][:url]="http://hgdownload.cse.ucsc.edu/admin/jksrc.zip"
-# 2015-10-09
-default[:ucscGenomeBrowser][:jksrc][:checksum]="940f4f2e3b181425399d3b9a3afc7b75ff229c4bef9d0942e0dd61d90ee4f087"
+# 2015-10-15
+default[:ucscGenomeBrowser][:jksrc][:checksum]="8eb2abd7ee73087a7d5c6c5fbb3054dd79e12d579e709c022eebbaf983de2f39"
 

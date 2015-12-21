68a8bff9b754ecf9f3ce59a6a169f9d3bb0e6f27
manabuishii <manabu.ishii.rb@gmail.com>
2015-12-21 13:50:49 +0900

    update checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:ucscGenomeBrowser][:jksrc][:url]="http://hgdownload.cse.ucsc.edu/admin/jksrc.zip"
-# 2015-11-27
-default[:ucscGenomeBrowser][:jksrc][:checksum]="27a0cdb9837d4dfaae2fbe7d3d8971830040a92bf47162f95bd617192e8afe16"
+# 2015-12-21
+default[:ucscGenomeBrowser][:jksrc][:checksum]="ce320bff057df674085b8dcfed76cab6e0663ea2e8576302b87dc9dd55e16782"
 

b608d159c91cdebad42428e50d3a4a11fe5f1a0f
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-04-22 16:51:46 +0900

    Update jksrc.zip sha256sum

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default[:ucscGenomeBrowser][:jksrc][:url]="http://hgdownload.cse.ucsc.edu/admin/jksrc.zip"
-# 2016-04-04
-default[:ucscGenomeBrowser][:jksrc][:checksum]="61c59dfc1671a8d0038d380f7fa833c9afb545668884f9b2d2e6037c3212d02b"
+# 2016-04-22
+default[:ucscGenomeBrowser][:jksrc][:checksum]="c24d16af0112f2240cb1b9efc70ac8f84ef089b443099551fe395de8d454d5bb"
 
